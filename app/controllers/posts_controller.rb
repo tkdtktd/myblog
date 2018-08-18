6a5e087c7550ae20f909ae8996ee85a65b603730
tkdtktd <tkdtktd3104@gmail.com>
2018-08-19 02:38:03 +0900

    add new files

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all.order(created_at:'desc')
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    # save
+    # @post = Post.new(params[:post])
+    @post = Post.new(post_params)
+    if @post.save
+      # redirect
+      redirect_to posts_path
+    else
+      # render plain: @post.errors.inspect
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
+end
