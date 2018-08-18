01753c8bd0cc9e2b1d66648ed0c3251486b2ffc7
tkdtktd <tkdtktd3104@gmail.com>
2018-08-19 04:45:20 +0900

    add new files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,6 +32,17 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
